b145e53fca1b4b75a4c11418bdd8716b43101d2b
Frank Wen <xbwen@hotmail.com>
2015-09-12 13:46:59 +0800

    add junit test for bugu-mongo-lucene

---
+++ bugu-mongo-lucene/src/test/java/com/bugull/mongo/lucene/entity/Product.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) www.bugull.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bugull.mongo.lucene.entity;
+
+import com.bugull.mongo.BuguEntity;
+import com.bugull.mongo.annotations.Entity;
+import com.bugull.mongo.annotations.Id;
+import com.bugull.mongo.annotations.Property;
+import com.bugull.mongo.lucene.annotations.IndexProperty;
+
+/**
+ * 商城产品
+ * 
+ * @author Frank Wen(xbwen@hotmail.com)
+ */
+@Entity
+public class Product implements BuguEntity {
+    
+    @Id
+    private String id;
+    @IndexProperty(analyze = true)
+    private String name;
+    @Property(lazy = true)
+    @IndexProperty
+    private String description;
+    private Float price;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+}
