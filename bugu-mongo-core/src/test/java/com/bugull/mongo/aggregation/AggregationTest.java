109ecb05811b5987e377cc7b2e8081efb900cd9a
Frank Wen <xbwen@hotmail.com>
2015-10-17 16:41:56 +0800

    add aggregation junit test code

---
+++ bugu-mongo-core/src/test/java/com/bugull/mongo/aggregation/AggregationTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) www.bugull.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bugull.mongo.aggregation;
+
+import com.bugull.mongo.BuguAggregation;
+import com.bugull.mongo.base.BaseTest;
+import com.mongodb.DBObject;
+import java.util.Date;
+import org.junit.Test;
+
+/**
+ *
+ * @author Frank Wen(xbwen@hotmail.com)
+ */
+public class AggregationTest extends BaseTest {
+    
+    /**
+     * insert 5 records, for aggregate operation.
+     */
+    @Test
+    public void testInsert(){
+        connectDB();
+        
+        BookDao dao = new BookDao();
+        
+        Book b1 = new Book();
+        b1.setTitle("About Java");
+        b1.setAuthor("Frank");
+        b1.setTags(new String[]{"Java", "Programming"});
+        b1.setPrice(50F);
+        b1.setPublishDate(new Date());
+        dao.save(b1);
+        
+        Book b2 = new Book();
+        b2.setTitle("About C++");
+        b2.setAuthor("Frank");
+        b2.setTags(new String[]{"C++", "Programming"});
+        b2.setPrice(40F);
+        b2.setPublishDate(new Date());
+        dao.save(b2);
+        
+        Book b3 = new Book();
+        b3.setTitle("About Android");
+        b3.setAuthor("Tom");
+        b3.setTags(new String[]{"Android", "Java", "C++", "Programming"});
+        b3.setPrice(60F);
+        b3.setPublishDate(new Date());
+        dao.save(b3);
+        
+        Book b4 = new Book();
+        b4.setTitle("About iPhone");
+        b4.setAuthor("Jessica");
+        b4.setTags(new String[]{"iPhone", "Objective-C", "Programming"});
+        b4.setPrice(70F);
+        b4.setPublishDate(new Date());
+        dao.save(b4);
+        
+        Book b5 = new Book();
+        b5.setTitle("About Network");
+        b5.setAuthor("Tom");
+        b5.setTags(new String[]{"Network"});
+        b5.setPrice(80F);
+        b5.setPublishDate(new Date());
+        dao.save(b5);
+        
+        disconnectDB();
+    }
+    
+    /**
+     * group by author, order by books count
+     */
+    @Test
+    public void testAggregation_1(){
+        connectDB();
+        
+        BookDao dao = new BookDao();
+        
+        BuguAggregation agg = dao.aggregate();
+        agg.group("{_id:'$author', count:{$sum:1}}");
+        agg.sort("{count:-1}");
+        Iterable<DBObject> it = agg.results();
+        for(DBObject dbo : it){
+            System.out.println(dbo.get("_id"));
+            System.out.println(dbo.get("count"));
+        }
+        disconnectDB();
+    }
+    
+    /**
+     * sum the total price of books contain tag 'Programming'
+     */
+    @Test
+    public void testAggregation_2(){
+        connectDB();
+        
+        BookDao dao = new BookDao();
+        
+        BuguAggregation agg = dao.aggregate();
+        agg.unwind("$tags");
+        agg.match("tags", "Programming");
+        agg.group("{_id:null, total:{$sum:'$price'}}");
+        Iterable<DBObject> it = agg.results();
+        for(DBObject dbo : it){
+            System.out.println(dbo.get("total"));
+        }
+        
+        disconnectDB();
+    }
+
+}
---
+++ bugu-mongo-core/src/test/java/com/bugull/mongo/aggregation/Book.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) www.bugull.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bugull.mongo.aggregation;
+
+import com.bugull.mongo.BuguEntity;
+import com.bugull.mongo.annotations.Entity;
+import com.bugull.mongo.annotations.Id;
+import java.util.Date;
+
+/**
+ *
+ * @author Frank Wen(xbwen@hotmail.com)
+ */
+@Entity
+public class Book implements BuguEntity {
+    
+    @Id
+    private String id;
+    private String title;
+    private String author;
+    private String[] tags;
+    private float price;
+    private Date publishDate;
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
+
+}
