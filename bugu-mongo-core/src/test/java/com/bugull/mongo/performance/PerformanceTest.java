496ff5165bd848a28720e242e07e7fe29f91cb1d
Frank Wen <xbwen@hadlinks.com>
2019-09-17 09:45:55 +0800

    add junit test

--- bugu-mongo-core/src/test/java/com/bugull/mongo/performance/PerformanceTest.java
+++ bugu-mongo-core/src/test/java/com/bugull/mongo/performance/PerformanceTest.java
@@ -19,6 +19,8 @@ import com.bugull.mongo.BuguDao;
 import com.bugull.mongo.base.ReplicaSetBaseTest;
 import com.bugull.mongo.cache.DaoCache;
 import com.bugull.mongo.entity.Order;
+import com.bugull.mongo.entity.User;
+import java.util.Date;
 import java.util.List;
 import org.junit.Test;
 
@@ -28,7 +30,7 @@ import org.junit.Test;
  */
 public class PerformanceTest extends ReplicaSetBaseTest{
     
-    @Test
+    //@Test
     public void testForProfile() {
         connectDB();
         
@@ -41,6 +43,32 @@ public class PerformanceTest extends ReplicaSetBaseTest{
                 
             }
         }
+        
+        disconnectDB();
+    }
+    
+    /**
+     * update operation use currentDate() and set() with new Date have no difference.
+     */
+    @Test
+    public void testCurrentDate(){
+        connectDB();
+        
+        BuguDao<User> dao = DaoCache.getInstance().get(User.class);
+        
+        long begin1 = System.currentTimeMillis();
+        for(int i=0; i<10000; i++){
+            dao.update().currentDate("registerTime").execute("5d7216f9661c3c6c1f83b943");
+        }
+        long end1 = System.currentTimeMillis();
+        System.out.println("use: " + (end1 - begin1));
+        
+        long begin2 = System.currentTimeMillis();
+        for(int i=0; i<10000; i++){
+            dao.update().set("registerTime", new Date()).execute("5d7216f9661c3c6c1f83b943");
+        }
+        long end2 = System.currentTimeMillis();
+        System.out.println("use: " + (end2 - begin2));
         
         disconnectDB();
     }
